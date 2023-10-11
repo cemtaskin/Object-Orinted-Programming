094f405649e573a37147978f0828e141c22bc15d
cemtaskin <cemtaskin82@gmail.com>
2023-10-11 17:26:54 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ali Duru!");
+            Console.WriteLine("Hello Okan University");
+
+            Console.ReadLine();
+        }
+    }
+}
