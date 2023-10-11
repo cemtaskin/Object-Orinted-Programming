e8a5bfe243878c1fe9985ac8b783ef1bd866ed76
cemtaskin <cemtaskin82@gmail.com>
2023-10-11 18:21:26 +0300

    Variables are created

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,14 +6,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //this line of code will print out a string
+            int myNum = 5; //we declare (create) a variable that stores integer values like (1,5,19) and it's name is myNum
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
 
-            /*
-            Console.WriteLine("Hello Ali Duru!");
-            Console.WriteLine("Hello Okan University");
-            */
-            Console.WriteLine(3 + 3);
-          
+
+            Console.WriteLine("We create an integer variable and its value is : " + myNum);
+            Console.WriteLine("We create an double variable and its value is : " + myDoubleNum);
+            Console.WriteLine("We create an boolean variable and its value is : " + myBool);
+            Console.WriteLine("We create an string variable and its value is : " + myText);
+            myNum = 20;
+            Console.WriteLine("We assign a new value to our integer variable and its new value is : " + myNum);
+
             Console.ReadLine();
         }
     }
